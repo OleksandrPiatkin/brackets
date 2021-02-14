1deb139ae68f27dcf63fd049d54b1ede0559faec
OleksandrPiatkin <Oleksandr.Piatkin@gmx.com>
2021-02-15 00:14:02 +0100

    feat: add refactored solution of similar task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let string = str;
+  const bracketsArray = [];
+  bracketsConfig.forEach(element => bracketsArray.push(element.join("")));
+
+  const recursion = () => {
+    if (string.length === 0) {
+      return true;
+    }
+    for (const element of bracketsArray) {
+      if (string.includes(element)) {
+        string = string.replace(element, "");
+        return recursion();
+      }
+    }
+    return false;
+  };
+  return recursion();
+};
